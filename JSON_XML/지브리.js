b0756cbb14cde287aab534e997c39a737f18954e
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-09 23:59:48 +0900

    Create 지브리.js

---
+++ JSON_XML/지브리.js
@@ -0,0 +1,17 @@
+const a = {
+  "지브리OST리스트" : [
+    {
+      "name" : "마녀 배달부 키키",
+      "song" : "따스함에 둘러쌓인다면"
+    },
+    {
+      "name" : "하울의 움직이는 성",
+      "song" : "세계의 약속"
+    }    
+  ]
+}
+
+console.log(a.지브리OST리스트)
+console.log(a.지브리OST리스트[0])
+console.log(a.지브리OST리스트[0].name)
+console.log(a.지브리OST리스트[0]["song"])
